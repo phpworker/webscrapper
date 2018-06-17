eb8a0c31f131de014970e6e4ca7b4ca7f3bf3276
Chris Kepinski <phpworker@gmail.com>
2018-06-17 19:42:31 +0200

    trying to make webscrapping work...

--- app/Http/Controllers/Youtube2Mp3Controller.php
+++ app/Http/Controllers/Youtube2Mp3Controller.php
@@ -2,15 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\VarDumper\VarDumper;
+use Weidner\Goutte\GoutteFacade as Goutte;
 
 class Youtube2Mp3Controller extends Controller
 {
     public function index($url)
     {
+        $results = array();
+        $url = 'https://slide.ly/promo/collections?mode=search&keyword=' . $url;
+        $headerXpath = '/*div[contains(@class, "header show opaque")]';
+        $videoXpath = '//div[contains(@class, "pr-video-preview-wrap")]/video';
+
+        $crawler = Goutte::request('GET', $url);
+
+        // this return an error, didn't figure it why yet.
+//        var_dump($crawler->filter($headerXpath)->count());
+//        die;
+
+        // for some reason this get the header of the page instead of content
+//        $crawler->filterXPath($videoXpath);
+//        var_dump($crawler->html());
+//        die;
+
+//        $crawler->filter($videoXpath)->each(function($node) {
+//
+//        });
+
+
         return response()->json([
             'status' => 'true',
-            'url' => $url
+            'url' => $url,
+            'video_id' => $results
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::get('/youtube2mp3/{url}', 'Youtube2Mp3Controller@index');
+Route::get('/youtube2mp3/{url}', 'Youtube2Mp3Controller@index')->name('youtube2mp3');
